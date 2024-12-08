47e5a80c5150e044639717bd3f976a1d3ff569aa
Piyush <piyushsainiyt868@gmail.com>
2024-12-08 20:35:30 +0530

    changes

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
-const celsiusEl = document.getElementById("celsius");
-const fahrenheitEl = document.getElementById("fahrenheit");
-const kelvinEl = document.getElementById("kelvin");
+const celsius1 = document.getElementById("celsius");
+const fahrenheit1 = document.getElementById("fahrenheit");
+const kelvin1 = document.getElementById("kelvin");
 
 function computeTemp(event) {
   const currentValue = +event.target.value;
 
   switch (event.target.name) {
     case "celsius":
-      kelvinEl.value = (currentValue + 273.32).toFixed(2);
-      fahrenheitEl.value = (currentValue * 1.8 + 32).toFixed(2);
+      kelvin1.value = (currentValue + 273.32).toFixed(2);
+      fahrenheit1.value = (currentValue * 1.8 + 32).toFixed(2);
       break;
     case "fahrenheit":
-      celsiusEl.value = ((currentValue - 32) / 1.8).toFixed(2);
-      kelvinEl.value = ((currentValue - 32) / 1.8 + 273.32).toFixed(2);
+      celsius1.value = ((currentValue - 32) / 1.8).toFixed(2);
+      kelvin1.value = ((currentValue - 32) / 1.8 + 273.32).toFixed(2);
       break;
     case "kelvin":
-      celsiusEl.value = (currentValue - 273.32).toFixed(2);
-      fahrenheitEl.value = ((currentValue - 273.32) * 1.8 + 32).toFixed(2);
+      celsius1.value = (currentValue - 273.32).toFixed(2);
+      fahrenheit1.value = ((currentValue - 273.32) * 1.8 + 32).toFixed(2);
       break;
     default:
       break;
